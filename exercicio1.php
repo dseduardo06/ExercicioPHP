7bc960a59eb24f42a4ea398ee3a9d528ffe6ce9d
dseduardo06 <ds.eduardo06@gmail.com>
2026-09-14 23:13:07 -0300

    exercicio1

---
+++ exercicio1.php
@@ -0,0 +1,15 @@
+<?php
+
+$cor = "vermelho";
+
+if ($cor == "vermelho") {
+    echo "Pare!";
+} elseif ($cor == "amarelo") {
+    echo "Atenção!";
+} elseif ($cor == "verde") {
+    echo "Siga!";
+} else {
+    echo "Cor inválida.";
+}
+
+?>
